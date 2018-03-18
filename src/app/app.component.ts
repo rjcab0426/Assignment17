bbf84a8664d0eb2cbd2c4c1905762264dab39f6c
Reman <you@example.com>
2018-03-18 10:52:47 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  displayRedBox = true;
+  listOfCourses = [
+    'Angular',
+    'JavaScript',
+    'NodeJS'
+  ]
+toggleDisplay() {
+    this.displayRedBox = !this.displayRedBox;
+  }
+
 }
